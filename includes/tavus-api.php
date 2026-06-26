1e19b35ea8a0eebde52a0c76ef5916e8f247227e
Bailey Frye <baileyafrye@comcast.net>
2026-06-26 17:26:17 -0500

    Started adding api endpoints

--- includes/tavus-api.php
+++ includes/tavus-api.php
@@ -5,9 +5,17 @@ if (!defined('ABSPATH')) {
 }
 
 class Tavus_API {
-    private $base_url = "https://tavusapi.com/v2";
+    private $baseUrl = "https://tavusapi.com/v2";
     public function __construct()
     {
         throw new \Exception('Not implemented');
+    }
+
+    public function fetchVideos() {
+        return wp_remote_get($this->baseUrl . "/videos");
+    }
+
+    public function fetchVideo(string $videoId) {
+        return wp_remote_get($this->baseUrl . "/videos/$videoId");
     }
 }
