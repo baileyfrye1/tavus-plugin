f1161b242137e13c81b4d5836669b7fea8989be0
baileyfrye1 <baileyafrye@comcast.net>
2026-06-26 14:19:07 -0500

    Initial commit

---
+++ tavus-integration.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * 
+ * Plugin Name: Tea on THC Tavus Integration
+ * Description: Custom plugin to integrate Tavus with Tea on THC website
+ * Author: Bailey Frye
+ * Version: 0.1.1
+ *
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class Tavus_Main {
+    public function __construct()
+    {
+        throw new \Exception('Not implemented');
+    }
+}
