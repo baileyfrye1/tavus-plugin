415974c379cd0d52e913655f6f4baa7135f73b0f
baileyfrye1 <baileyafrye@comcast.net>
2026-06-26 16:12:49 -0500

    committing to work on other machine

---
+++ includes/tavus-api.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class Tavus_API {
+    private $base_url = "https://tavusapi.com/v2";
+    public function __construct()
+    {
+        throw new \Exception('Not implemented');
+    }
+}
